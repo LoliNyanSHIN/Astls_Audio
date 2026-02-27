439a85bbb04cfd126bf4fd909fc608ff36dff5ab
LoliNyanSHIN <kokushi1405x404@gmail.com>
2026-02-27 15:35:50 +0900

    rep

---
+++ main/js/playlist.js
@@ -0,0 +1,7 @@
+window.serverMusicList = [
+    {
+        name: "アイデン貞貞メルトダウン",
+        artist: " ",
+        url: "https://pan.xxbyq.net/f/gKoMFe/01.%20%E3%82%A2%E3%82%A4%E3%83%87%E3%83%B3%E8%B2%9E%E8%B2%9E%E3%83%A1%E3%83%AB%E3%83%88%E3%83%80%E3%82%A6%E3%83%B3.flac"
+    },
+];
